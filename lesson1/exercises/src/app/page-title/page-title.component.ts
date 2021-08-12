9b19abcc0947e382d8e038c8bee1480e169832a3
Ilyane <ilyaneroey@gmail.com>
2021-08-12 17:14:36 -0500

    added optional final touches

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-  title = "Stuff";
+  title = "Stuff I Do";
   constructor() { }
 
   ngOnInit() {
